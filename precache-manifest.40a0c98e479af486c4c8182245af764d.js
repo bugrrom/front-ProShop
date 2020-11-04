f42703d3a32020b53a9512737923efb08ab6bd45
Roman <bugrom@mail.ru>
2020-11-04 15:57:10 +0300

    Updates

---
+++ precache-manifest.40a0c98e479af486c4c8182245af764d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c9a338600aca40d52d0ecbfa6333b7",
+    "url": "/front-ProShop/index.html"
+  },
+  {
+    "revision": "93c9a9acc5bd0c648929",
+    "url": "/front-ProShop/static/js/2.7dd1e5d9.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/front-ProShop/static/js/2.7dd1e5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "176d958114461e297c63",
+    "url": "/front-ProShop/static/js/main.893e8fd5.chunk.js"
+  },
+  {
+    "revision": "139d9c09ae3adbad7a90",
+    "url": "/front-ProShop/static/js/runtime-main.91f39f33.js"
+  }
+]);
